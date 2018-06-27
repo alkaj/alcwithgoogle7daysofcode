c2d76042275db6f0939f6a3fa449f09081276d4c
alkaj <alkaj09@gmail.com>
2018-06-27 20:08:38 +0100

    initial commit

---
+++ app/src/main/java/com/walkity/apps/journalapp/DiaryActivity.java
@@ -0,0 +1,13 @@
+package com.walkity.apps.journalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
---
+++ app/src/main/java/com/walkity/apps/journalapp/DiaryFactoryActivity.java
@@ -0,0 +1,13 @@
+package com.walkity.apps.journalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryFactoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_factory);
+    }
+}
