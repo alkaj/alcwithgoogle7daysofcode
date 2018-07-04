5d5ab49806e57b175ecceb10442dfc7a7443d266
alkaj <alkaj09@gmail.com>
2018-07-05 00:51:38 +0100

    fix: properly handling result for existing pictures

--- app/src/main/java/com/walkity/apps/journalapp/addeditdiary/DiaryFactoryActivity.java
+++ app/src/main/java/com/walkity/apps/journalapp/addeditdiary/DiaryFactoryActivity.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +20,8 @@ import com.walkity.apps.journalapp.R;
 import com.walkity.apps.journalapp.data.DiaryEntry;
 import com.walkity.apps.journalapp.databinding.ActivityDiaryFactoryBinding;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Locale;
 
 public class DiaryFactoryActivity extends AppCompatActivity implements AddEditContract.View {
@@ -160,12 +164,35 @@ public class DiaryFactoryActivity extends AppCompatActivity implements AddEditCo
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GET_IMAGE_REQUEST && resultCode == RESULT_OK)
+        if(requestCode == GET_IMAGE_REQUEST && resultCode == RESULT_OK && null != data)
         {
-            //then get the image...
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            //and load it into the preview...
-            loadImage(image);
+            Bitmap image = null;
+            if(data.getExtras() != null && data.hasExtra("data"))
+            {
+                //This is an image from the camera...
+
+                //Get the image...
+                image = (Bitmap) data.getExtras().get("data");
+            }
+            else if(data.getData() != null)
+            {
+                //This is an existing image...
+
+                //Get the image uri sent as data...
+                Uri imageUri = data.getData();
+                //Load it into an input stream
+                try {
+                    InputStream imageIS = getContentResolver().openInputStream(imageUri);
+                    //Get the image...
+                    image = BitmapFactory.decodeStream(imageIS);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+            //and load it into the preview... only if
+            if(null != image)
+                loadImage(image);
         }
     }
 
